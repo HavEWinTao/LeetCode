4561ee7c5083e3f9a632297d5ee3923c1a16b2a0
HavEWinTao <854540702@qq.com>
2021-10-17 14:26:55 +0800

    2.两数相加——2021.10.17

---
+++ 2.addTwoNumbers/cmake-build-debug/CMakeFiles/2_addTwoNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_addTwoNumbers.exe"
+  "2_addTwoNumbers.exe.manifest"
+  "2_addTwoNumbers.pdb"
+  "CMakeFiles/2_addTwoNumbers.dir/main.cpp.obj"
+  "lib2_addTwoNumbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_addTwoNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
