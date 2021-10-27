7087a572722a7c636540e2e35a48520d3ad5d419
HavEWinTao <854540702@qq.com>
2021-10-27 19:24:14 +0800

    7.Z字形变换——2021.10.26

---
+++ 6.convert/cmake-build-debug/CMakeFiles/6_convert.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_convert.exe"
+  "6_convert.exe.manifest"
+  "6_convert.pdb"
+  "CMakeFiles/6_convert.dir/main.cpp.obj"
+  "lib6_convert.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_convert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
