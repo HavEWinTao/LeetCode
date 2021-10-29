388cef0a488d7dc1137b0356173b9471c0133e09
HavEWinTao <854540702@qq.com>
2021-10-29 12:01:43 +0800

    9.回文数——2021.10.29

---
+++ 9.isPalindrome/cmake-build-debug/CMakeFiles/9_isPalindrome.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9_isPalindrome.exe"
+  "9_isPalindrome.exe.manifest"
+  "9_isPalindrome.pdb"
+  "CMakeFiles/9_isPalindrome.dir/main.cpp.obj"
+  "lib9_isPalindrome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_isPalindrome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
