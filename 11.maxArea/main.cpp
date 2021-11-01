ae45198f0d710e53c8f117bc25dc370f6cf9f211
HavEWinTao <854540702@qq.com>
2021-11-01 14:38:18 +0800

    11.盛水最多的容器——2021.11.1

--- 11.maxArea/main.cpp
+++ 11.maxArea/main.cpp
@@ -4,19 +4,38 @@ using namespace std;
 
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-
+    int maxArea(vector<int> &height) {
+        int size = height.size();
+        int left_index = 0;
+        int right_index = size - 1;
+        int ans = 0;
+        while (left_index < right_index) {
+            int temp = (right_index - left_index) * min(height[left_index], height[right_index]);
+            if (temp > ans) {
+                ans = temp;
+            }
+            if (height[left_index] < height[right_index]) {
+                left_index++;
+            } else {
+                right_index--;
+            }
+        }
+        return ans;
     }
 };
 
 int main() {
     Solution solution;
     vector<int> height;
-    height.push_back(4);
-    height.push_back(3);
-    height.push_back(2);
     height.push_back(1);
+    height.push_back(8);
+    height.push_back(6);
+    height.push_back(2);
+    height.push_back(5);
     height.push_back(4);
-    solution.maxArea(height);
+    height.push_back(8);
+    height.push_back(3);
+    height.push_back(7);
+    int ans = solution.maxArea(height);
     return 0;
 }
