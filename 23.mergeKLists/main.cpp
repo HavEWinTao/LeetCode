c2a6991f6ca122798be82966fc9ffca0c9f65aab
HavEWinTao <854540702@qq.com>
2021-11-23 19:02:39 +0800

    23.合并K个升序链表——2021.11.23

--- 23.mergeKLists/main.cpp
+++ 23.mergeKLists/main.cpp
@@ -16,8 +16,60 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *ans = new ListNode(0);
+        ListNode *rear = ans;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                ListNode *temp = new ListNode(l1->val);
+                rear->next = temp;
+                rear = rear->next;
+                l1 = l1->next;
+            } else {
+                ListNode *temp = new ListNode(l2->val);
+                rear->next = temp;
+                rear = rear->next;
+                l2 = l2->next;
+            }
+        }
+        if (l1) {
+            rear->next = l1;
+        }
+        if (l2) {
+            rear->next = l2;
+        }
+        return ans->next;
+    }
 
+    vector<ListNode *> recursion(vector<ListNode *> &lists) {
+        int size = lists.size();
+        vector<ListNode *> ans;
+        for (int i = 0; i < size / 2; i++) {
+            ListNode *temp = mergeTwoLists(lists[i], lists[size - i - 1]);
+            ans.push_back(temp);
+        }
+        if ((size % 2) == 1) {
+            ans.push_back(lists[size / 2]);
+        }
+        return ans;
+    }
+
+
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        int size = lists.size();
+        if (size == 0) {
+            return NULL;
+        }
+        ListNode *ans;
+        vector<ListNode *> temp = lists;
+        while (true) {
+            temp = recursion(temp);
+            if (temp.size() == 1) {
+                ans = temp[0];
+                break;
+            }
+        }
+        return ans;
     }
 };
 
