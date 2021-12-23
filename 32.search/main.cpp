c214c54928937323df38c133af42ab7a09104205
HavEWinTao <854540702@qq.com>
2021-12-23 15:49:47 +0800

    33.搜索旋转排序数组——2021.12.23

--- 32.search/main.cpp
+++ 32.search/main.cpp
@@ -6,40 +6,41 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int> &nums, int target) {
-        int ans = -1;
         int size = (int) nums.size();
-        int right = size;
+        if (size == 0) {
+            return -1;
+        }
+        if (size == 1) {
+            return (nums[0] == target) ? 0 : -1;
+        }
+        int right = size - 1;
         int left = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums[mid] > nums[left]) {//第一段升序区间里
-                if (target > nums[mid]) {//右面有两个升序序列，但是分开了两部分，第二部分比较小
-                    left = mid + 1;
-                } else if (target == nums[mid]) {//找到
-                    ans = mid;
-                    break;
-                } else {//在左区间内查找或者在第二个区间内查找
-                    right = mid - 1;
-                }
-            } else {//mid已经在第二个区间了
-                if (target > nums[mid] && target < nums[size - 1]) {//在第二个区间内
-                    left = mid + 1;
-                } else if (target > nums[mid] && target > nums[size - 1]) {//第一个区间内
-                    right = mid - 1;
-                } else if (target < nums[mid]) {
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {//mid在第一段升序区间
+                if (nums[0] <= target && target <= nums[mid]) {//在0~mid区间内
                     right = mid - 1;
                 } else {
-                    ans = mid;
+                    left = mid + 1;
+                }
+            } else {//mid在第二段升序区间
+                if (nums[mid] <= target && target <= nums[size - 1]) {//在mid~size-1的区间内
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
         }
-        return ans;
+        return -1;
     }
 };
 
 int main() {
     Solution solution;
-    vector<int> nums{4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums{1, 3};
     int ans = solution.search(nums, 0);
     return 0;
 }
