74d0dd9e50eed7e48ca8dcc9d65c05006636216e
HavEWinTao <854540702@qq.com>
2022-01-06 15:47:12 +0800

    TODO:今天写完

---
+++ 30.findSubstring/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+    }
+};
+
+int main() {
+    string s="barfoothefoobarman";
+    vector<string> words;
+    words.emplace_back("foo");
+    words.emplace_back("bar");
+    return 0;
+}
