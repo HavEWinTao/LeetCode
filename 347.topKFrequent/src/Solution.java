53ce45d38da9a51e659a2a1645cdb7a01a2c17c3
HavEWinTao <854540702@qq.com>
2022-03-24 10:39:07 +0800

    2022-3-24

---
+++ 347.topKFrequent/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        List<Integer>[] list = new List[nums.length + 1];
+        for (int key : map.keySet()) {
+            int i = map.get(key);
+            if (list[i] == null) {
+                list[i] = new ArrayList<>();
+            }
+            list[i].add(key);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = list.length - 1; i > 0 && ans.size() < k; i--) {
+            if (list[i] != null) {
+                ans.addAll(list[i]);
+            }
+        }
+        return ans;
+    }
+}
