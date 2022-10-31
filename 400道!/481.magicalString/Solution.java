ec0b2e7e056a4f7e32907909ef4a7959eead620d
HavEWinTao <854540702@qq.com>
2022-10-31 13:54:34 +0800

    2022.10.21~31

--- 400道!/481.magicalString/Solution.java
+++ 400道!/481.magicalString/Solution.java
@@ -4,15 +4,21 @@ public class Solution {
         int[] str = new int[100001];
         str[0] = 1;
         str[1] = 2;
-        int i=1,j=1;
+        int i = 1, j = 1;
         int flag = 1;
-        while(i<100001){
+        int ans = 1;
+        while (i < n) {
             int num = str[j];
-            for (int k=0;k<num;k++){
-                str[i++] = 1+flag;
+            for (int k = 0; k < num && i < n; k++) {
+                str[i] = 1 + flag;
+                if (str[i] == 1) {
+                    ans++;
+                }
+                i++;
             }
-            flag = flag==1?0:1;
+            flag = flag == 1 ? 0 : 1;
             j++;
         }
+        return ans;
     }
 }
