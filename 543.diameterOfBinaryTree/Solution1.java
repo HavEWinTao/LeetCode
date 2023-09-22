4310784071e3c5bc6380d26942e0b5ee20d83004
fanhongtao <854540702@qq.com>
2023-09-23 00:03:27 +0800

    2023.9.22

---
+++ 543.diameterOfBinaryTree/Solution1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    private int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ 782.minDiffInBST/Solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    private int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        dfs(root, -1, Integer.MAX_VALUE);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int left, int right) {
+        if (node == null) return;
+        if (left != -1) {
+            ans = Math.min(ans, Math.abs(node.val - left));
+        }
+        if (right != Integer.MAX_VALUE) {
+            ans = Math.min(ans, Math.abs(right - node.val));
+        }
+        dfs(node.left, left, node.val);
+
+        dfs(node.right, node.val, right);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        {
+            TreeNode root = new TreeNode(4);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(6);
+            root.left.left = new TreeNode(1);
+            root.left.right = new TreeNode(3);
+            int ans = solution.minDiffInBST(root);
+            System.out.println(ans);
+        }
+        {
+            TreeNode root = new TreeNode(90);
+            root.left = new TreeNode(69);
+            root.left.left = new TreeNode(49);
+            root.left.right = new TreeNode(89);
+            root.left.left.right = new TreeNode(52);
+            int ans = solution.minDiffInBST(root);
+            System.out.println(ans);
+        }
+    }
+}
